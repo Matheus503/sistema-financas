a83fcc209f075dc547b1330286bc75847bd7edaa
Matheus503 <matheus.gomesmoreira@gmail.com>
2026-04-28 21:37:56 -0300

    fix: move dynamic to top

--- src/app/extrato/page.tsx
+++ src/app/extrato/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useCallback, useEffect, useMemo, useState, Suspense } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { auth } from "../../lib/auth";
