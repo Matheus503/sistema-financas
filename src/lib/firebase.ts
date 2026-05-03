5c2dd849443817c5a81d585cfd55acfaa43c16d8
Matheus503 <matheus.gomesmoreira@gmail.com>
2026-05-03 15:12:29 -0300

    configura ambientes firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAMvM08QovvDx3ox9pLeqT632reeFvk9CI",
-  authDomain: "sistema-financas.firebaseapp.com",
-  projectId: "sistema-financas",
-  storageBucket: "sistema-financas.firebasestorage.app",
-  messagingSenderId: "1004458226419",
-  appId: "1:1004458226419:web:7ba8b55e9f75666f593d86",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 };
 
 export const app = initializeApp(firebaseConfig);
